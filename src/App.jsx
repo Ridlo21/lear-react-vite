de9a730bd3061f2c1a2afe11e35b6541e297538a
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2023-08-20 04:03:45 +0700

    coba socket io

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import { io, Manager } from "socket.io-client"
+
 
 export default function App() {
     const [message, setMessage] = useState("")
 
     const messageList = document.getElementById('messageList')
 
-    const sendMessage = (e) => {
+    const sendMessage = async (e) => {
         e.preventDefault()
         if (message) {
             let chatList = document.createElement('p')
             chatList.style.fontSize = '16px'
             chatList.textContent = message
             messageList.appendChild(chatList)
+            await fetch(`http://localhost:4001/v1/socket/forecast?count=${message}`).then(all => {
+                console.log(all);
+            })
             setMessage("")
         }
     }
+
+    useEffect(() => {
+        const socket = io('http://localhost:4001');
+        socket.on('connect', (data) => {
+            console.log('socket connected');
+        });
+
+        socket.on('mod_forecast', data => {
+            console.log(data);
+        })
+    }, [])
 
     return (
         <div className={'mx-auto mt-40 w-80 border shadow-md h-80 px-4 bg-slate-50'}>
